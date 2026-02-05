655b74864d5de00ef649fdf099e4587587b180e5
CarlosCosa-Prog <ciclosm@localhost>
2026-02-05 13:43:44 +0100

    Subir Actividad 1 Unidad 4

--- UD4/Act1_Ud4/ejercicio2/ejercicio2/Form1.Designer.cs
+++ UD4/Act1_Ud4/ejercicio2/ejercicio2/Form1.Designer.cs
@@ -28,12 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            panel1 = new Panel();
+            button2 = new Button();
+            button1 = new Button();
+            richTextBox2 = new RichTextBox();
+            richTextBox1 = new RichTextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(richTextBox2);
+            panel1.Controls.Add(richTextBox1);
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(293, 499);
+            panel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button2.Location = new Point(13, 442);
+            button2.Name = "button2";
+            button2.Size = new Size(261, 23);
+            button2.TabIndex = 7;
+            button2.Text = "Generar PDF";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button1.Location = new Point(13, 413);
+            button1.Name = "button1";
+            button1.Size = new Size(261, 23);
+            button1.TabIndex = 6;
+            button1.Text = "Seleccionar foto";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            richTextBox2.Location = new Point(13, 301);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(261, 96);
+            richTextBox2.TabIndex = 5;
+            richTextBox2.Text = "";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            richTextBox1.Location = new Point(13, 187);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(261, 96);
+            richTextBox1.TabIndex = 4;
+            richTextBox1.Text = "";
+            // 
+            // textBox4
+            // 
+            textBox4.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox4.Location = new Point(13, 143);
+            textBox4.Name = "textBox4";
+            textBox4.PlaceholderText = "Dirección";
+            textBox4.Size = new Size(261, 23);
+            textBox4.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox3.Location = new Point(13, 99);
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "Teléfono";
+            textBox3.Size = new Size(261, 23);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox2.Location = new Point(13, 57);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Correo electrónico";
+            textBox2.Size = new Size(261, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Location = new Point(13, 13);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Nombre";
+            textBox1.Size = new Size(261, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(293, 499);
+            Controls.Add(panel1);
+            MinimumSize = new Size(309, 538);
+            Name = "Form1";
+            Text = "Generador de PDF";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Button button2;
+        private Button button1;
+        private RichTextBox richTextBox2;
+        private RichTextBox richTextBox1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
     }
 }
--- UD4/Act1_Ud4/ejercicio2/ejercicio2/Form1.cs
+++ UD4/Act1_Ud4/ejercicio2/ejercicio2/Form1.cs
@@ -1,10 +1,211 @@
+using iText.IO.Image;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Draw;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+using Document = iText.Layout.Document;
+using Color = System.Drawing.Color;
 namespace ejercicio2
 {
     public partial class Form1 : Form
     {
+        string fotoRuta = "";   // defino un string con la ruta de la foto
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Configurar el placeholder en los richTextBox
+            SetPlaceholder(richTextBox1, "Educación");
+            SetPlaceholder(richTextBox2, "Experiencia laboral");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Cuadro de dialogo que permite seleccionar la imagen 
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Archivos de imagen (*.jpg; *.jpeg; *.png)|*.jpg; *.jpeg; *.png";
+                openFileDialog.Title = "Seleccionar una foto";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    fotoRuta = openFileDialog.FileName;
+                    MessageBox.Show("Foto seleccionada correctamente.", "Foto cargada", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Si la carpeta no existe, la crea
+            /*string carpeta = @"C:\pdf";
+            if (!Directory.Exists(carpeta))
+            {
+                Directory.CreateDirectory(carpeta);
+            }
+
+            // Ruta donde se guardará el PDF
+            string rutaPDF = Path.Combine(carpeta, "curriculumPrueba.pdf");*/
+
+            // Selector de ubicación para guardar el PDF
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Archivos PDF (*.pdf)|*.pdf";
+                saveFileDialog.Title = "Guardar Currículum como PDF";
+                saveFileDialog.FileName = "curriculum.pdf"; // nombre por defecto
+
+                if (saveFileDialog.ShowDialog() != DialogResult.OK)
+                {
+                    // Si el usuario cancela, salir de la función
+                    return;
+                }
+
+                string rutaPDF = saveFileDialog.FileName;
+
+                // Crear un documento PDF
+                using (var pdfWriter = new PdfWriter(rutaPDF))
+                {
+                    using (var pdf = new PdfDocument(pdfWriter))
+                    {
+                        // Crear un documento iText
+                        var document = new Document(pdf);
+
+                        // Agregar cabecera al documento
+                        Paragraph cabecera = new Paragraph("Currículum Vitae");
+                        cabecera.SetTextAlignment(TextAlignment.CENTER);
+                        cabecera.SetFontSize(20);
+                        document.Add(cabecera);
+
+                        // Agregar linea separadora
+                        LineSeparator lineaSeparadora = new LineSeparator(new SolidLine());
+                        document.Add(lineaSeparadora);
+
+                        // Agregar imagen
+                        // Si la ruta es correcta crea el Image y lo añade a al document
+                        if (File.Exists(fotoRuta))
+                        {
+                            ImageData imageData = ImageDataFactory.Create(fotoRuta);
+                            iText.Layout.Element.Image imagen =
+                                new iText.Layout.Element.Image(imageData);
+
+                            imagen.SetHorizontalAlignment(iText.Layout.Properties.HorizontalAlignment.CENTER);
+                            imagen.SetWidth(200);
+                            imagen.SetMarginTop(10);
+
+                            document.Add(imagen);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Para crear el PDF, primero debes seleccionar tu foto");
+                        }
+
+                        // Campos de texto
+                        // Validar campo Nombre
+                        /*if (!ValidarCampo(textBox1, "Nombre"))
+                            return;*/
+
+                        // Agregar nombre
+                        Paragraph nombre = new Paragraph("Nombre: " + textBox1.Text);
+                        nombre.SetTextAlignment(TextAlignment.LEFT);
+                        nombre.SetFontSize(15);
+                        document.Add(nombre);
+
+                        // Validar campo correo
+                        /*if (!ValidarCampo(textBox2, "Correo"))
+                            return;*/
+                        // Agregar correo
+                        Paragraph correo = new Paragraph("Correo: " + textBox2.Text);
+                        correo.SetTextAlignment(TextAlignment.LEFT);
+                        correo.SetFontSize(12);
+                        document.Add(correo);
+
+                        // Validar campo correo
+                        /*if (!ValidarCampo(textBox3, "Teléfono"))
+                            return;*/
+                        // Agregar correo
+                        Paragraph telefono = new Paragraph("Teléfono: " + textBox3.Text);
+                        telefono.SetTextAlignment(TextAlignment.LEFT);
+                        telefono.SetFontSize(12);
+                        document.Add(telefono);
+
+                        // Validar campo correo
+                        /*if (!ValidarCampo(textBox4, "Dirección"))
+                            return;*/
+                        // Agregar correo
+                        Paragraph direccion = new Paragraph("Dirección: " + textBox4.Text);
+                        direccion.SetTextAlignment(TextAlignment.LEFT);
+                        direccion.SetFontSize(12);
+                        document.Add(direccion);
+
+                        // Agregar otra linea separadora
+                        document.Add(lineaSeparadora);
+                        // Agregar el parrafo de Educación
+                        Paragraph educacionTitulo = new Paragraph("Educación:");
+                        educacionTitulo.SetFontSize(16);
+                        document.Add(educacionTitulo);
+                        Paragraph educacion = new Paragraph(richTextBox1.Text);
+                        document.Add(educacion);
+
+                        // Agregar otra linea separadora
+                        document.Add(lineaSeparadora);
+                        // Agregar el parrafo de Educación
+                        Paragraph expLaboralTitulo = new Paragraph("Experiencia laboral:");
+                        expLaboralTitulo.SetFontSize(16);
+                        document.Add(expLaboralTitulo);
+                        Paragraph expLaboral = new Paragraph(richTextBox2.Text);
+                        document.Add(expLaboral);
+
+                    }
+                }
+
+                // Mensaje informando que el PDF se creó
+                MessageBox.Show($"PDF generado correctamente en:\n{rutaPDF}", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        // Funcion que comprueba que un campo no esté vacio (no es necesaria en esta actividad)
+        /*
+        private bool ValidarCampo(TextBox textBox, string nombreCampo)
+        {
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                MessageBox.Show(
+                    $"El campo \"{nombreCampo}\" no puede estar vacío",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }*/
+
+        // Función para configurar placeholder en los richTextBox
+        private void SetPlaceholder(RichTextBox rtb, string placeholderText)
+        {
+            rtb.Text = placeholderText;
+            rtb.ForeColor = Color.Gray;
+
+            rtb.GotFocus += (s, e) =>
+            {
+                if (rtb.Text == placeholderText)
+                {
+                    rtb.Text = "";
+                    rtb.ForeColor = Color.Black; // color normal
+                }
+            };
+
+            rtb.LostFocus += (s, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(rtb.Text))
+                {
+                    rtb.Text = placeholderText;
+                    rtb.ForeColor = Color.Gray;
+                }
+            };
         }
     }
 }
